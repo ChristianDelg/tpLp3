d548de4d1ab3d6b657e8d9a819ca276ac5be0082
Christian Delgado <ing.delgadomontiel@gmail.com>
2016-09-27 02:11:07 -0400

    h
    Signed-off-by: Christian Delgado <ing.delgadomontiel@gmail.com>

--- tp3.c
+++ tp3.c
@@ -1,71 +1,59 @@
+/* cc sig_talk.c -o sig_talk  */
+
 #include <stdio.h>
-#include <stdlib.h>
 #include <signal.h>
-/* Variable global con el pid del proceso hijo */
-int pid = 0;
-/* Rutina de tratamiento del mensaje SIGINT desde donde se va a mandar el mensaje
-al proceso hijo para que éste muera */
-void gestor_SIGINT( int señal )
-{
-    printf(“Rutina de tratamiento de SIGINT\n”);
-    if( pid != 0 )
-    {
-        kill( pid, SIGUSR1 );
-        exit( 0 );
+#include <stdlib.h>
+#include <unistd.h>
+
+void sighup(); /* routines child will call upon sigtrap */
+void sigint();
+void sigquit();
+
+int main()
+{ int pid;
+
+  /* get child process */
+  
+   if ((pid = fork()) < 0) {
+        perror("fork");
+        exit(1);
     }
+    
+   if (pid == 0)
+     { /* child */
+       signal(SIGHUP,sighup); /* set function calls */
+       signal(SIGINT,sigint);
+       signal(SIGQUIT, sigquit);
+       for(;;); /* loop for ever */
+     }
+  else /* parent */
+     {  /* pid hold id of child */
+       printf("\nPARENT: sending SIGHUP\n\n");
+       kill(pid,SIGHUP);
+       sleep(3); /* pause for 3 secs */
+       printf("\nPARENT: sending SIGINT\n\n");
+       kill(pid,SIGINT);
+       sleep(3); /* pause for 3 secs */
+       printf("\nPARENT: sending SIGQUIT\n\n");
+       kill(pid,SIGQUIT);
+       sleep(3);
+     }
 }
-void gestor_hijo( int señal )
-{
-    exit(0);
+
+void sighup()
+
+{  signal(SIGHUP,sighup); /* reset signal */
+   printf("CHILD: I have received a SIGHUP\n");
 }
-main( void )
-{
-    int codigo_error=0;
-    struct sigaction gestion;
-    /* Primero se instala el gestor de la señal SIGINT */
-    gestion.sa_handler = gestor_SIGINT;
-    gestion.sa_mask = 0;
-    gestion.sa_flags = SA_ONESHOT;
-    codigo_error = sigaction ( SIGINT, gestion, 0);
-    if( codigo_error == SIG_ERR )
-    {
-        perror(“Error al definir el gestor de SIGINT”);
-        exit(-1);
-    }
-    if( (pid = fork()) == -1 )
-    {
-        printf(“Error en la creación del hijo\n”);
-        exit( 1 );
-    }
-    else if( pid == 0 )
-    {
-        /* Código del hijo */
-        /* Primero se desactiva la respuesta a SIGINT */
-        gestion.sa_handler = SIG_IGN;
-        gestion.sa_mask = 0;
-        gestion.sa_flags = SA_ONESHOT;
-        codigo_error = sigaction ( SIGINT, gestion, 0);
-        if( codigo_error == SIG_ERR )
-        {
-            perror(“Error al definir el gestor de SIGINT”);
-            exit(-1);
-        }
-        /* Segundo se instala el gestor de SIGUSR1 */
-        gestion.sa_handler = gestor_hijo;
-        gestion.sa_mask = 0;
-        gestion.sa_flags = SA_ONESHOT;
-        codigo_error = sigaction ( SIGUSR1, gestion, 0);
-        if( codigo_error == SIG_ERR )
-        {
-            perror(“Error al definir el gestor de SIGUSR1”);
-            exit(-1);
-        }
-        /* Código */
-        while(1)
-    }
-    else
-    {
-        /* Código del padre */
-        while(1);
-    }
+
+void sigint()
+
+{  signal(SIGINT,sigint); /* reset signal */
+   printf("CHILD: I have received a SIGINT\n");
+}
+
+void sigquit()
+
+{ printf("My DADDY has Killed me!!!\n");
+  exit(0);
 }
